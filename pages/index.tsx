c9793930fed1744eb93084af5a528cf28376e6e2
InnaLazarenko <lazarenkofreelance@gmail.com>
2023-03-20 11:19:08 +0200

    add relative path2

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,7 @@
 import Head from './components/Head';
 import Main from '../components/common/Main';
 
-import Hero from 'components/hero/Hero';
+import Hero from '../components/hero/Hero';
 
 export default function Home() {
   return (
