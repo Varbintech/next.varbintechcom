203ff962b062fe69d8e522c7e2c0102585b12792
var-bin <var-bin@users.noreply.github.com>
2023-03-27 20:14:10 +0300

    7-pricing-section-21.03.2023: fix aria-label attribute

--- components/common/buttons/ButtonClose.tsx
+++ components/common/buttons/ButtonClose.tsx
@@ -3,15 +3,14 @@ import type { FC } from 'react';
 import CloseIcon from '@mui/icons-material/Close';
 import IconButton, { type IconButtonProps } from '@mui/material/IconButton';
 
-// if ButtonCloseComponent will be used more times, 'const' change to 'let'
-const iconButtonId = 0;
+let iconButtonId = 0;
 
 const ButtonCloseComponent: FC<IconButtonProps> = props => {
   const { 'aria-label': ariaLabel, size, ...restProps } = props;
 
   return (
-    <IconButton {...restProps} aria-label={ariaLabel || `icon-close-${iconButtonId}`}>
-      <CloseIcon fontSize={size || 'medium'} />
+    <IconButton {...restProps} aria-label={ariaLabel || `icon-button-${iconButtonId++}`}>
+      <CloseIcon fontSize={size || 'medium'} aria-label={`close-icon-${iconButtonId}`} />
     </IconButton>
   );
 };
