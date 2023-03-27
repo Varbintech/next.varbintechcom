369958845be8146820fcb054ce98e896c1ae4966
var-bin <var-bin@users.noreply.github.com>
2023-03-27 15:59:00 +0300

    hotfix/serve-static-assets-cache-policy: configure caching for immutable assets

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,5 @@
+import type { GetServerSideProps } from 'next';
+
 import Head from './components/Head';
 
 import Main from '../components/common/Main';
@@ -8,6 +10,8 @@ import Services from '../components/services/Services';
 
 import { caseStudies } from '../mocks/case-study';
 import { services } from '../mocks/services';
+
+import { Settings } from '../constants/settings';
 
 export default function Home() {
   return (
@@ -24,3 +28,15 @@ export default function Home() {
     </>
   );
 }
+
+export const getServerSideProps: GetServerSideProps = async ({ res }) => {
+  res.setHeader(
+    'Cache-Control',
+    // @TODO think about stale-while-revalidate=${Settings.RevalidateTime}
+    `public, max-age=${Settings.MaxAge}, immutable`,
+  );
+
+  return {
+    props: {},
+  };
+};
