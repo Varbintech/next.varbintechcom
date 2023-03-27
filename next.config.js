245f05931a5a022aa4fb92c2701c1d2c137abe75
var-bin <var-bin@users.noreply.github.com>
2023-03-27 22:16:45 +0300

    bugfix/lighthouse-report-serve-static-assets-cache-policy: new try

--- next.config.js
+++ next.config.js
@@ -1,12 +1,30 @@
 /**
  * @type {import('next').NextConfig}
  **/
+
+const headers = async () => {
+  return [
+    {
+      source: '/:all*(svg|jpg|png|webp)',
+      locale: false,
+      headers: [
+        {
+          key: 'Cache-Control',
+          value: 'public, max-age=31536000, immutable',
+        },
+      ],
+    },
+  ];
+};
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
     minimumCacheTTL: 60,
   },
+
+  headers,
 };
 
 module.exports = nextConfig;
