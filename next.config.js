a582fbf023e512118286be30b8b7229bea2e21f8
var-bin <var-bin@users.noreply.github.com>
2023-03-29 14:59:23 +0300

    bugfix/lighthouse-report-images-a11y-shift: change logic for unoptimized images inside next.js config

--- next.config.js
+++ next.config.js
@@ -25,7 +25,7 @@ module.exports = async (phase, { defaultConfig: _ }) => {
     swcMinify: true,
     images: {
       minimumCacheTTL: 60,
-      unoptimized: phase === PHASE_PRODUCTION_BUILD && phase !== process.env.CI,
+      unoptimized: phase === PHASE_PRODUCTION_BUILD && !process.env.CI,
     },
 
     headers,
