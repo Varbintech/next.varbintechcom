5458bcb37b318d5409777540281d10f2494f4e44
InnaLazarenko <lazarenkofreelance@gmail.com>
2023-04-04 13:14:29 +0300

    add layout

--- pages/case-studies/index.tsx
+++ pages/case-studies/index.tsx
@@ -1,42 +1,21 @@
-import Head from '../components/Head';
-
-import Main from '../../components/common/Main';
-import Navigation from '../../components/common/navigation/Navigation';
-
 import Hero from '../../components/hero/Hero';
 import TabsFilter from '../../components/common/tabsFilter/TabsFilter';
 import CaseStudies from '../../components/case-studies/CaseStudies';
-import Contact from '../../components/contact/Contact';
-import Footer from '../../components/footer/Footer';
-// import Questions from '../../components/questions/Questions';
 
 import { caseStudies } from '../../mocks/case-study';
-import { socialIcons } from '../../mocks/social-icons';
-import { footerData } from '../../mocks/footer-data';
 import { tabsItems } from '../../mocks/tabs-items';
-// import { questions } from '../../mocks/questions';
 
 const CaseStudiesPage = () => {
   return (
     <>
-      <Head />
-
-      <Navigation />
-
-      <Main>
-        <Hero
-          small
-          title="Case studies that we feel proud"
-          subtitle="We help startups and companies of all shapes and sizes to build high-quality Front-End
-          solutions."
-        />
-        <TabsFilter data={tabsItems} />
-        <CaseStudies data={caseStudies} />
-        {/* @TODO uncomment when FAQ ia ready */}
-        {/* <Questions data={questions} /> */}
-        <Contact data={socialIcons} />
-        <Footer data={footerData} />
-      </Main>
+      <Hero
+        small
+        title="Case studies that we feel proud"
+        subtitle="We help startups and companies of all shapes and sizes to build high-quality Front-End
+        solutions."
+      />
+      <TabsFilter data={tabsItems} />
+      <CaseStudies data={caseStudies} />
     </>
   );
 };
