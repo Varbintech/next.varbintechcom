c176d310e78cb71a2e42723f986365b6fedb82a5
var-bin <var-bin@users.noreply.github.com>
2023-04-04 12:43:35 +0300

    51-case-studies-page-03.04.2023: remove commented out code

--- pages/case-studies/index.tsx
+++ pages/case-studies/index.tsx
@@ -8,13 +8,11 @@ import TabsFilter from '../../components/common/tabsFilter/TabsFilter';
 import CaseStudies from '../../components/case-studies/CaseStudies';
 import Contact from '../../components/contact/Contact';
 import Footer from '../../components/footer/Footer';
-// import Questions from '../../components/questions/Questions';
 
 import { caseStudies } from '../../mocks/case-study';
 import { socialIcons } from '../../mocks/social-icons';
 import { footerData } from '../../mocks/footer-data';
 import { tabsItems } from '../../mocks/tabs-items';
-// import { questions } from '../../mocks/questions';
 
 const CaseStudiesPage = () => {
   return (
@@ -32,8 +30,6 @@ const CaseStudiesPage = () => {
         />
         <TabsFilter data={tabsItems} />
         <CaseStudies data={caseStudies} />
-        {/* @TODO uncomment when FAQ ia ready */}
-        {/* <Questions data={questions} /> */}
         <Contact data={socialIcons} />
         <Footer data={footerData} />
       </Main>
