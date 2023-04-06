98d3ae1131ca8ea11ab68c7af4afd8e05f3d3d74
var-bin <var-bin@users.noreply.github.com>
2023-04-06 20:51:06 +0300

    62-email-template-06.04.2023: fix template displaying in Storybook

--- email-template/ConfirmationEmail.stories.tsx
+++ email-template/ConfirmationEmail.stories.tsx
@@ -1,16 +1,5 @@
 import { storiesOf } from '@storybook/react';
 
-import { render } from '@react-email/render';
-
 import ConfirmationEmail from './ConfirmationEmail';
 
-const html = render(<ConfirmationEmail />, {
-  pretty: true,
-  /* plainText: true, */
-});
-
-export const Template = () => {
-  return <>{html}</>;
-};
-
-storiesOf('App/EmailTemplates/Confirmation', module).add('Default', () => <Template />);
+storiesOf('App/EmailTemplates/Confirmation', module).add('Default', () => <ConfirmationEmail />);
