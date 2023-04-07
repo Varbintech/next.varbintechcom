cc35a713bcb75dce2ec8ff22e8ffa0b4590d6f10
var-bin <var-bin@users.noreply.github.com>
2023-04-08 00:12:39 +0300

    feature/send-emails-issue-63: apply middleware to avoid CORS issues

--- pages/api/send-email.ts
+++ pages/api/send-email.ts
@@ -1,5 +1,7 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
 
+import Cors from 'cors';
+
 import nodemailer, { type SendMailOptions } from 'nodemailer';
 
 // Gmail
@@ -41,7 +43,30 @@ const mailData = (
   };
 };
 
-export default function (req: NextApiRequest, res: NextApiResponse) {
+// Initializing the cors middleware
+// You can read more about the available options here: https://github.com/expressjs/cors#configuration-options
+const cors = Cors({
+  methods: ['POST', 'GET', 'HEAD'],
+});
+
+// Helper method to wait for a middleware to execute before continuing
+// And to throw an error when an error happens in a middleware
+function runMiddleware(req: NextApiRequest, res: NextApiResponse, fn: (...arg: any) => any) {
+  return new Promise((resolve, reject) => {
+    fn(req, res, (result: any) => {
+      if (result instanceof Error) {
+        return reject(result);
+      }
+
+      return resolve(result);
+    });
+  });
+}
+
+export default async function (req: NextApiRequest, res: NextApiResponse) {
+  // Run the middleware
+  await runMiddleware(req, res, cors);
+
   transporter.sendMail(mailData(req.body.name, req.body.to, req.body.message), (err, info) => {
     if (err) {
       console.error(err);
