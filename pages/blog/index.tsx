1ae386c8e53200aa8ff535bee34cf0f2905064fe
InnaLazarenko <lazarenkofreelance@gmail.com>
2023-04-10 12:19:33 +0300

    add index page

---
+++ pages/blog/index.tsx
@@ -0,0 +1,11 @@
+import Hero from '../../components/hero/Hero';
+
+const BlogPage = () => {
+  return (
+    <>
+      <Hero small title="Stay inspired Varbin tech Blog" />
+    </>
+  );
+};
+
+export default BlogPage;
