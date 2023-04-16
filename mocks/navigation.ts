e209e5b88cec55c34f77f0747719f0dad51db4d7
var-bin <var-bin@users.noreply.github.com>
2023-04-16 14:14:52 +0300

    refactor: blog route is available only development env

--- mocks/navigation.ts
+++ mocks/navigation.ts
@@ -4,5 +4,10 @@ export const navigationRoutes: Array<NavigationRoute> = [
   { id: 0, text: 'Case studies', href: '', scrollTo: '#caseStudies' },
   { id: 1, text: 'Services', href: '', scrollTo: '#services' },
   { id: 2, text: 'Pricing', href: '', scrollTo: '#pricing' },
-  { id: 3, text: 'Blog', href: '/blog', scrollTo: '' },
+  {
+    id: 3,
+    text: 'Blog',
+    href: process.env.NODE_ENV === 'development' ? '/blog' : '',
+    scrollTo: '',
+  },
 ];
