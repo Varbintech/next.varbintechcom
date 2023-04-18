929df6e6b28a7b66fdc578d08c22b6b237a5befc
var-bin <var-bin@users.noreply.github.com>
2023-04-19 00:34:52 +0300

    chore: change props and add comment

--- components/dialogs/checkout/DialogCheckout.stories.tsx
+++ components/dialogs/checkout/DialogCheckout.stories.tsx
@@ -3,10 +3,19 @@ import { action } from '@storybook/addon-actions';
 
 import DialogCheckout from './DialogCheckout';
 
+/**
+ * Testing the checkout
+ *
+ * https://docs.lemonsqueezy.com/guides/developer-guide/testing-going-live#testing-the-checkout
+ */
+
 const onClose = (...args: any) => {
   action('Close button was clicked')(...args);
 };
 
-storiesOf('App/Dialog/CustomServices', module).add('Default', () => (
-  <DialogCheckout onClose={onClose} />
+storiesOf('App/Dialog/Checkout', module).add('Default', () => (
+  <DialogCheckout
+    onClose={onClose}
+    checkoutSrc={String(process.env.NEXT_PUBLIC_LEMONSQUEEZY_CHECKOUT_TEST_URL_QUARTER)}
+  />
 ));
