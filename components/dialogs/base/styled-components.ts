646613dd4db19000e28547924badbca87ab0c5f4
var-bin <var-bin@users.noreply.github.com>
2023-04-19 00:26:02 +0300

    feat: introduce a list of styled components for DialogsCheckout

--- components/dialogs/base/styled-components.ts
+++ components/dialogs/base/styled-components.ts
@@ -22,9 +22,9 @@ export const DialogBaseTitle = styled('div')(({ theme }) => ({
   },
 }));
 
-export const StyledMuiDialog = styled(Dialog)(({ theme }) => ({
+export const StyledMuiDialog = styled(Dialog)(({ theme, fullScreen, fullWidth }) => ({
   '& .MuiPaper-root': {
-    width: '570px',
+    width: fullScreen || fullWidth ? '100%' : '570px',
     borderRadius: '8px',
     boxShadow: 'none',
 
@@ -47,5 +47,47 @@ export const StyledMuiDialog = styled(Dialog)(({ theme }) => ({
     [theme.breakpoints.up('md')]: {
       padding: '8px 40px 40px',
     },
+  },
+}));
+
+export const StyledIFrame = styled('iframe')`
+  z-index: 2147483647;
+  display: block;
+  background-color: transparent;
+  border: 0px none transparent;
+  overflow-x: hidden;
+  overflow-y: auto;
+  visibility: visible;
+  margin: 0px;
+  padding: 0px;
+  -webkit-tap-highlight-color: transparent;
+  position: absolute;
+  left: 0;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  width: 100%;
+  height: 100%;
+  flex-grow: 1;
+  flex-shrink: 0;
+`;
+
+export const IFrameContainer = styled('div')`
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  height: 100%;
+  justify-content: space-around;
+`;
+
+export const IFrameLoadingContainer = styled('div')(({ theme }) => ({
+  display: 'flex',
+  flexDirection: 'column',
+  height: '100%',
+  justifyContent: 'space-around',
+  padding: '0 8px',
+
+  [theme.breakpoints.up('md')]: {
+    padding: '0 40px',
   },
 }));
