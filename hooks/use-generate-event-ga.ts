19f64ccc1466c8cae38c26c6efdc0d44ef95279e
var-bin <var-bin@users.noreply.github.com>
2023-04-30 14:57:22 +0300

    chore: remove console.warn

--- hooks/use-generate-event-ga.ts
+++ hooks/use-generate-event-ga.ts
@@ -5,8 +5,6 @@ import { generateEvent } from '../lib/gtag';
 export const useGenerateEventGa = (element: 'button' | 'link') => {
   const handleClick: MouseEventHandler<HTMLButtonElement | HTMLAnchorElement> = useCallback(
     event => {
-      console.warn('handleClick: ', element, event.currentTarget.id);
-
       generateEvent(element, event.currentTarget.id);
     },
     [element],
