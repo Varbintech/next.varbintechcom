029ab297f824ed016df1b65fc5dd9757afff968b
var-bin <var-bin@users.noreply.github.com>
2023-05-05 14:50:32 +0300

    fix(code-review): add new value for author; change texts and image

--- constants/meta.ts
+++ constants/meta.ts
@@ -1,13 +1,16 @@
 export const MetaData = {
-  Title: 'Varbintech | Development Company | Get started',
-  Description: 'Top-notch JavaScript experience and software development for your needs',
+  Title: 'Varbintech | High-quality Front-End solutions | Get started',
+  Description:
+    "We help startups and companies of any size to craft exceptional Front-End solutions that set you apart. From an idea to the final launch, we've got you covered.",
   IndexKeywords:
-    'Startup, Front-end Development, Software Development, JavaScript, TypeScript, React, Angular',
+    'Startup, Front-end Development, Software Development, JavaScript, TypeScript, React, Angular, Landing page, Web app development, Code optimization, Startup-friendly front-end development',
+  IndexAuthor: 'Varbintech',
+  IndexType: 'website', // Use article for articles and website for the rest of your pages.
   Url: 'https://varbintech.com',
   Lang: 'en',
   Locale: 'en_us',
   Image:
-    'https://res.cloudinary.com/dlxpwfaic/image/upload/v1680439761/varbintech/varbintech-join-the-club_qypktu.jpg',
-  ImageAlt: 'Varbintech | Development Company | Get started',
+    'https://res.cloudinary.com/dlxpwfaic/image/upload/v1683287110/varbintech/OG-general_zaxpjl.jpg',
+  ImageAlt: 'Varbintech | High-quality Front-End solutions | Get started',
   ImageType: 'image/jpeg',
 } as const;
