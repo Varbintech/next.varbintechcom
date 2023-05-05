cbd58a974858e4711247303395af67376f5b826e
var-bin <var-bin@users.noreply.github.com>
2023-05-05 14:49:25 +0300

    fix(code-review): add author meta tag and some comments

--- components-pages/head/HeadIndex.tsx
+++ components-pages/head/HeadIndex.tsx
@@ -6,15 +6,18 @@ const HeadIndex = () => {
   return (
     <NextHead>
       <title>{MetaData.Title}</title>
-
+      {/* General */}
       <meta name="description" content={MetaData.Description} />
       <meta name="keywords" content={MetaData.IndexKeywords} />
       <meta name="image" content={MetaData.Image} />
+      <meta name="author" content={MetaData.IndexAuthor} />
 
+      {/* Google / Search Engine Tags */}
       <meta itemProp="name" content={MetaData.Title} />
       <meta itemProp="description" content={MetaData.Description} />
       <meta itemProp="image" content={MetaData.Image} />
 
+      {/* Open Graph */}
       <meta property="og:site_name" content={MetaData.Title} />
       <meta property="og:title" content={MetaData.Title} />
       <meta property="og:description" content={MetaData.Description} />
@@ -24,6 +27,7 @@ const HeadIndex = () => {
       <meta property="og:image:type" content={MetaData.ImageType} />
       <meta property="og:image:alt" content={MetaData.ImageAlt} />
 
+      {/* Twitter */}
       <meta name="twitter:card" content="summary_large_image" />
       <meta name="twitter:title" content={MetaData.Title} />
       <meta name="twitter:description" content={MetaData.Description} />
