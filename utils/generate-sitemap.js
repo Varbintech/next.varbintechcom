b69f748173697287391cc7b4474ce939c491ea2b
var-bin <var-bin@users.noreply.github.com>
2023-05-06 15:12:39 +0300

    refactor: replace writeFileSync with writeFile to be able to show the inform message

--- utils/generate-sitemap.js
+++ utils/generate-sitemap.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const globby = require('globby');
 
+const FILENAME = 'sitemap.xml';
+
 function addPage(page) {
   const path = page.replace('pages', '').replace(/(.page.tsx|.page.ts)/, '');
   const route = path === '/index' ? '' : path;
@@ -20,11 +22,17 @@ async function generateSitemap() {
     '!pages/**/*-wip.{ts,tsx}',
     '!pages/api',
   ]);
-  const sitemap = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  const fileData = `<urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
   ${pages.map(addPage).join('\n')}
 </urlset>`;
 
-  fs.writeFileSync('public/sitemap.xml', sitemap);
+  fs.writeFile(`public/${FILENAME}`, fileData, err => {
+    if (err) {
+      throw err;
+    }
+
+    console.warn(`> [SEO] ${FILENAME} has been created!`);
+  });
 }
 
 generateSitemap();
