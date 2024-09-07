469b725301c014d04ece8d4506019a8418fcbf49
Vitalii Rybka <var-bin@users.noreply.github.com>
2024-09-07 15:43:06 +0300

    feat: implement new componet for markdown

--- constants/inter-latin.ts
+++ constants/inter-latin.ts
@@ -1,5 +1,9 @@
-import { Inter } from '@next/font/google';
+import { Inter, Fira_Code } from '@next/font/google';
 
 export const inter = Inter({
+  subsets: ['latin'],
+});
+
+export const firaCode = Fira_Code({
   subsets: ['latin'],
 });
