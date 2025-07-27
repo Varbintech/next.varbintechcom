15f889e7a8062452da7e76fa3866e85d5775772c
Vitalii Rybka <var-bin@users.noreply.github.com>
2025-07-27 20:57:51 +0300

    feat: add autoscroll (scroll adjustment) for table of content when the table of content is longer than scren size

--- components/common/table-of-content/TableOfContent.tsx
+++ components/common/table-of-content/TableOfContent.tsx
@@ -1,5 +1,7 @@
 import { useEffect, useState, useRef } from 'react';
 
+import { useRouter } from 'next/router';
+
 import Typography from '@mui/material/Typography';
 import Stack from '@mui/material/Stack';
 import Box from '@mui/material/Box';
@@ -8,23 +10,16 @@ import type { ProjectImage, SocialIcon } from '../../../models';
 import type { TocContent } from '../../../utils/api.blog';
 
 import ImageWrapperComponent from '../image-wrapper/ImageWrapper';
-import SocialIcons from '../social-icons/SocialIcons';
-import Link from '../link/Link';
 
-import { TableOfContentContainer } from './styled-components';
+import SocialIcons from '../social-icons/SocialIcons';
+
+import { TableOfContentContainer, TableOfContentFixedHeight } from './styled-components';
+import TocLink from './TocLink';
 
 interface TableOfContentProps {
   image: ProjectImage;
   data: Array<TocContent>;
   socialIcons: Array<SocialIcon>;
-  slug: string;
-}
-
-interface TocLinkProps {
-  href: string;
-  title: string;
-  slug: string;
-  activeId: string;
 }
 
 const options = {
@@ -33,20 +28,63 @@ const options = {
   threshold: [],
 };
 
-const TocLink = ({ href, title, activeId }: TocLinkProps) => {
-  return (
-    <Link href={`#${href}`} underline={activeId === href ? 'always' : 'none'} component="a">
-      {title}
-    </Link>
-  );
-};
-
 const TableOfContent = (props: TableOfContentProps) => {
-  const { image, socialIcons, data, slug } = props;
+  const { image, socialIcons, data } = props;
 
+  const router = useRouter();
   const [activeId, setActiveId] = useState('');
   const sections = useRef<NodeListOf<Element> | null>(null);
   const observer = useRef<IntersectionObserver | null>(null);
+  const tocContainerRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    let timeoutId = undefined;
+
+    if (activeId && tocContainerRef.current && router.isReady) {
+      const activeLink = tocContainerRef.current.querySelector(`a[href="#${activeId}"]`);
+
+      if (activeLink) {
+        const container = tocContainerRef.current;
+        const containerRect = container.getBoundingClientRect();
+        const linkRect = activeLink.getBoundingClientRect();
+
+        // @TODO: Issue with too many links in the table of contents
+        // WHen a user clicks on a link, the page scrolls to the wrong section,
+        // the wrong link is highlighted as active,
+        // `setTimeout` as a quick dirty fix
+
+        const isAboveViewport = linkRect.top < containerRect.top;
+        const isBelowViewport = linkRect.bottom > containerRect.bottom;
+
+        timeoutId = setTimeout(() => {
+          if (isAboveViewport || isBelowViewport) {
+            activeLink.scrollIntoView({
+              behavior: 'smooth',
+              block: 'center',
+            });
+          }
+        }, 200);
+      }
+    }
+
+    return () => {
+      if (timeoutId) {
+        clearTimeout(timeoutId);
+      }
+    };
+  }, [activeId, router.isReady]);
+
+  useEffect(() => {
+    if (router.isReady) {
+      const hash = router.asPath.split('#')[1];
+
+      if (hash) {
+        setActiveId(hash);
+      } else {
+        setActiveId('');
+      }
+    }
+  }, [router.isReady, router.asPath]);
 
   useEffect(() => {
     const timeoutId = setTimeout(() => {
@@ -65,6 +103,10 @@ const TableOfContent = (props: TableOfContentProps) => {
       });
     }, 100);
 
+    if (activeId === '') {
+      setActiveId(data[0].id);
+    }
+
     return () => {
       if (observer.current) {
         sections.current?.forEach(section => {
@@ -78,33 +120,41 @@ const TableOfContent = (props: TableOfContentProps) => {
         clearTimeout(timeoutId);
       }
     };
-  }, []);
+  }, [activeId, router, data]);
 
   return (
     <TableOfContentContainer>
-      <Box marginBottom="35px">
-        <ImageWrapperComponent data={image} smallSize />
-      </Box>
+      <TableOfContentFixedHeight ref={tocContainerRef}>
+        <Box marginBottom={4}>
+          <Typography variant="h5" marginBottom={1.75}>
+            Share article on:
+          </Typography>
 
-      <Typography variant="h5" marginBottom={1.75}>
-        Table of Contents
-      </Typography>
+          <Box marginBottom={4}>
+            <SocialIcons data={socialIcons} />
+          </Box>
 
-      <Stack spacing={2} marginBottom="52px">
-        {data.length > 0
-          ? data.map((item, index) => (
-              <TocLink
-                key={index}
-                href={item.href}
-                title={`${item?.title}`}
-                slug={slug}
-                activeId={activeId}
-              />
-            ))
-          : null}
-      </Stack>
+          <ImageWrapperComponent data={image} smallSize />
+        </Box>
 
-      <SocialIcons data={socialIcons} />
+        <Typography variant="h5" marginBottom={1.75}>
+          Table of Contents
+        </Typography>
+
+        <Stack spacing={2} marginBottom="52px">
+          {data.length > 0
+            ? data.map((item, index) => (
+                <TocLink
+                  key={`${item.id}-${index}`}
+                  isActive={activeId === item.id}
+                  href={item.id}
+                  title={item.title}
+                  level={item.level}
+                />
+              ))
+            : null}
+        </Stack>
+      </TableOfContentFixedHeight>
     </TableOfContentContainer>
   );
 };
