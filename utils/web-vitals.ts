bbd1efaca3040a34a7e6a104cb8e851d6bccd24e
Vitalii Rybka <var-bin@users.noreply.github.com>
2025-10-11 17:26:10 +0300

    feat: implement sending Core Web Vitals to GA

---
+++ utils/web-vitals.ts
@@ -0,0 +1,31 @@
+import type { NextWebVitalsMetric } from 'next/app';
+
+export function sendToGA({ id, name, label, value }: NextWebVitalsMetric) {
+  const categoryWebVitals = 'Web Vitals';
+  const categoryNextVitals = 'Next.js Vitals';
+  const isWebVitalLabel = label === 'web-vital';
+  const isCLS = name === 'CLS';
+  const isLCP = name === 'LCP';
+  const isINP = name === 'INP';
+
+  const prepareValue = (v: number) => {
+    if (isCLS) {
+      return Math.round(v * 1000); // multiply by 1000 to convert to milliseconds
+    }
+
+    if (isLCP || isINP) {
+      return Math.round(v); // already in milliseconds
+    }
+
+    return v;
+  };
+
+  // Assumes the global `gtag()` function exists, see:
+  // https://developers.google.com/analytics/devguides/collection/gtagjs
+  window.gtag('event', name, {
+    event_category: isWebVitalLabel ? categoryWebVitals : categoryNextVitals,
+    value: prepareValue(value),
+    event_label: id, // id unique to current page load
+    non_interaction: true, // avoids affecting bounce rate.
+  });
+}
